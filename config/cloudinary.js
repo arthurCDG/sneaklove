2425cefa76f020a435874e70efb75af9b6978f8e
Arthur Chastenet <arthur.chastenet@gmail.com>
2022-01-14 15:32:31 +0100

    Finished CRUD

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -2,7 +2,6 @@ const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const multer = require("multer");
 
-
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_KEY,
@@ -12,13 +11,12 @@ cloudinary.config({
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
-    allowed_formats: ['jpg', "jpeg", 'png', "gif"],
-    folder: 'sneaklove' // The name of the folder in cloudinary
+    allowed_formats: ["jpg", "jpeg", "png", "gif", "jfif"],
+    folder: "sneaklove", // The name of the folder in cloudinary
     // resource_type: 'raw' => this is in case you want to upload other type of files, not just images
-  }
+  },
 });
 
 const fileUploader = multer({ storage });
-
 
 module.exports = fileUploader;
--- routes/dashboard_sneaker.js
+++ routes/dashboard_sneaker.js
@@ -1,57 +1,66 @@
 const express = require("express"); // import express in this module
 const router = new express.Router(); // create an app sub-module (router)
-const SneakerModel = require("./../models/Sneaker")
-const uploader = require('./../config/cloudinary')
+const SneakerModel = require("./../models/Sneaker");
+const uploader = require("./../config/cloudinary");
 
 router.get("/", (req, res, next) => {
-    SneakerModel.find()
-        .then(sneakers => res.render("products_manage", { sneakers }))
-.catch((err) => next(err))
+  SneakerModel.find()
+    .then((sneakers) => res.render("products_manage", { sneakers }))
+    .catch((err) => next(err));
 });
 
 router.get("/create", (req, res, next) => {
-    res.render("products_add")
+  res.render("products_add");
 });
 
 router.get("/edit/:id", (req, res, next) => {
-    SneakerModel.findById(req.params.id)
-        .then((sneaker) => {
-            res.render("product-edit", { sneaker })
-        })
-        .catch((err) => next(err))
+  SneakerModel.findById(req.params.id)
+    .then((sneaker) => {
+      res.render("product_edit", { sneaker });
+    })
+    .catch((err) => next(err));
 });
 
 router.get("/delete/:id", (req, res, next) => {
-    SneakerModel.findByIdAndDelete(req.params.id)
-        .then(() => {
-            res.redirect("/dashboard")
-                .catch((err) => next(err))
-        })
+  SneakerModel.findByIdAndDelete(req.params.id)
+    .then(() => res.redirect("/dashboard"))
+    .catch((err) => next(err));
 });
 
 router.post("/create", uploader.single("image"), async (req, res, next) => {
-    const newSneaker = { ...req.body }
-    console.log("THIS IS THE BODY", req.body);
-    console.log("THIS IS THE FILE", req.file);
-    if (!req.file) newSneaker.image = undefined
-    else newSneaker.image = req.file.path
-    try {
-        await SneakerModel.create(newSneaker)
-        req.flash("success", "product successfully created")
-        res.redirect("/dashboard")
-    } catch (err) {
-        next(err)
-    }
-})
+  const newSneaker = { ...req.body };
+  console.log("THIS IS THE BODY", req.body);
+  console.log("THIS IS THE FILE", req.file);
+  if (!req.file) newSneaker.image = undefined;
+  else newSneaker.image = req.file.path;
+  try {
+    await SneakerModel.create(newSneaker);
+    req.flash("success", "product successfully created");
+    res.redirect("/dashboard");
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.post("/edit/:id", async (req, res, next) => {
-    try {
-        SneakerModel.findByIdAndUpdate(req.params.id, req.body)
-        req.flash("success", "product successfully updated")
-        res.redirect("/dashboard")
-    } catch (err) {
-        next(err)
-    }
-})
+router.post("/edit/:id", uploader.single("image"), async (req, res, next) => {
+  try {
+    const sneakerToUpdate = await { ...req.body };
+    if (!req.file) sneakerToUpdate.image = undefined;
+    else sneakerToUpdate.image = req.file.path;
+
+    console.log("1 - Do you go there??????????");
+
+    await SneakerModel.findByIdAndUpdate(req.params.id, sneakerToUpdate);
+
+    console.log("2 - Do you go there??????????");
+
+    req.flash("success", "product successfully updated");
+    res.redirect("/dashboard");
+
+    console.log("3 - Do you go there??????????");
+  } catch (err) {
+    next(err);
+  }
+});
 
 module.exports = router;
